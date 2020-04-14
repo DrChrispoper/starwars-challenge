7172b20bb023170464d7af3956a9f5f230c50816
Christopher Hockley <christopher@marketingmaps.co.uk>
2020-04-14 09:25:30 +0100

    Updates

---
+++ precache-manifest.92c01a17718100ca9dfe009952ac8ffc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "baa6c4ab93d81972e11fd89d249974a1",
+    "url": "/DrChrispoper/starwars-challenge/index.html"
+  },
+  {
+    "revision": "f5b93067a8e57b79afe9",
+    "url": "/DrChrispoper/starwars-challenge/static/css/main.1b3a1791.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/DrChrispoper/starwars-challenge/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DrChrispoper/starwars-challenge/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5b93067a8e57b79afe9",
+    "url": "/DrChrispoper/starwars-challenge/static/js/main.1937aee7.chunk.js"
+  },
+  {
+    "revision": "34e2551ca0ba38531686",
+    "url": "/DrChrispoper/starwars-challenge/static/js/runtime-main.f5146186.js"
+  }
+]);
